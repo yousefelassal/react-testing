4437310c7666f40dbfe75b827abcbab224120b8b
Yousef <yousefsameh24@gmail.com>
2023-08-04 18:20:25 +0300

    first query

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,24 +1,10 @@
 import { describe, it, expect } from 'vitest';
-import { render, screen } from '@testing-library/react';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-import App from './App';
-
-describe('App', () => {
-  it('renders headline', () => {
-    render(<App title="React" />);
-
-    screen.debug();
-
-    // check if App components renders headline
-  });
-});
-
-describe('something truthy and falsy', () => {
-  it('true to be true', () => {
-    expect(true).toBe(true);
-  });
-
-  it('false to be false', () => {
-    expect(false).toBe(false);
+describe("App component", () => {
+  it("renders correct heading", () => {
+    render(<App />);
+    expect(screen.getByRole("heading").textContent).toMatch(/our first test/i);
   });
 });
