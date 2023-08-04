529cd5ab853b3f8fd38fb50408477fd1ca2373fb
Yousef <yousefsameh24@gmail.com>
2023-08-04 17:50:05 +0300

    setup react testing library

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,17 @@
 import { describe, it, expect } from 'vitest';
+import { render, screen } from '@testing-library/react';
+
+import App from './App';
+
+describe('App', () => {
+  it('renders headline', () => {
+    render(<App title="React" />);
+
+    screen.debug();
+
+    // check if App components renders headline
+  });
+});
 
 describe('something truthy and falsy', () => {
   it('true to be true', () => {
